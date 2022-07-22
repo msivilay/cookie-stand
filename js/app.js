05f486fdba726bc9e54bce2a088288695d036ce8
Marina Sivilay <msivilay@gmail.com>
2022-07-21 22:24:55 -0700

    Replaced object literals with constructor function Store()

--- js/app.js
+++ js/app.js
@@ -1,47 +1,4 @@
-let seattle = {
-  location: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCookiePerCust: 6.3,
-  dataByHour: [],
-  getNumCust: numCustPerHour,
-};
-
-let tokyo = {
-  location: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avgCookiePerCust: 1.2,
-  dataByHour: [],
-  getNumCust: numCustPerHour,
-};
-
-let dubai = {
-  location: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  avgCookiePerCust: 3.7,
-  dataByHour: [],
-  getNumCust: numCustPerHour,
-};
-
-let paris = {
-  location: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  avgCookiePerCust: 2.3,
-  dataByHour: [],
-  getNumCust: numCustPerHour,
-};
-
-let lima = {
-  location: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  avgCookiePerCust: 4.6,
-  dataByHour: [],
-  getNumCust: numCustPerHour,
-};
+'use strict';
 
 // Min-Max inclusive random number generator from MDN
 function getRandomIntInclusive(min, max) {
@@ -50,6 +7,15 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
   //The maximum is inclusive and the minimum is inclusive
 }
+
+function Store(location, minCust, maxCust, avgCookiePerCust){
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiePerCust = avgCookiePerCust;
+  this.dataByHour = [];
+}
+
 
 function numCustPerHour(){
   return getRandomIntInclusive (this.minCust, this.maxCust);
